ba633830adb8e0f959158854b91095123e04005e
dsch-iter <dsch.iter@gmail.com>
2014-03-25 01:09:22 -0700

    first commit

---
+++ src/test/java/com/albion/RPN/RPNTest.java
@@ -0,0 +1,30 @@
+package test.java.com.albion.RPN;
+
+import main.java.com.albion.RPN.Solution;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class RPNTest {
+
+	@DataProvider(name ="data01")
+	public Object[][] evalRPNDataProvider() {
+
+		return new Object[][] {
+				{
+					new String[]{"2", "1", "+", "3", "*"}, 9
+				}, 
+				{
+					new String[]{"4", "13", "5", "/", "+"}, 6
+				}
+		};
+	}
+
+	@Test(dataProvider="data01")
+	public void evalRPN(String[] tokens, int expectedValue) {
+		Solution sol = new Solution();
+		int actualValue = sol.evalRPN(tokens);
+		Assert.assertEquals(actualValue, expectedValue);
+	}
+}
